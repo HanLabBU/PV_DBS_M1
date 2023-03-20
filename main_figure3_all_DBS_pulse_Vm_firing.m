00facc4750656b9698a5ba10ab0c37ec11418837
Pier Fier <fabrispierre@gmail.com>
2023-03-20 15:28:18 -0400

    Cleaned up some formatting and trying to figure out what certain variables mean

--- main_figure3_all_DBS_pulse_Vm_firing.m
+++ main_figure3_all_DBS_pulse_Vm_firing.m
@@ -26,15 +26,14 @@ for ind=[1:length(ses)]
         
         Cpath=ses(ind).name;
         load(Cpath) % Loading of the data file
-       %%%
+   
         FS=828;
         Fn = FS/2;FB=[ 86.5 88.5];
         [B, A] = butter(2, [min(FB)/Fn max(FB)/Fn]);
         LFPg= ((filtfilt(B,A,    result.traces(:,1))));
         result.traces(:,1)= result.traces(:,1)-LFPg;
-        %%%
-        ind
-        lfp=[];  asA=[];clear aV allV8 allV40 allV140 allS alls1
+        lfp=[];  asA=[];
+        clear aV allV8 allV40 allV140 allS alls1;
         for  ne=unique(result.trial_vec)
             v= result.traces(result.trial_vec==ne,1)./result.tracesB(result.trial_vec==ne)   ;
             v=v(1:2500);
@@ -64,62 +63,62 @@ for ind=[1:length(ses)]
             samp(spx <10)=NaN;
             asA=[asA, samp];
         end
+
         for id=1:2 %:size(vsig,2)
             lfp.label{id}= num2str(id);
         end   
         %%%%
         %%%%5%%%
-        MS1=[];  VS1=[];
-        for tr=1:size( alls1,2)
+        MS1=[];  
+        VS1=[];
+        for tr=1:size(alls1,2)
             try
                 opto=lfp2.trial{tr}(2,1:end);
                 windS=25;
                 trigs=find(diff(opto)>0.2);
                 for ind=1:length(trigs)-1
-                    if trigs(ind)-windS>0  & trigs(ind) +windS < size(alls1,1) & (trigs(ind+1)-trigs(ind))> 5 & trigs(ind)<FS
+                    if trigs(ind)-windS>0  & trigs(ind) +windS < size(alls1,1) & (trigs(ind+1)-trigs(ind))> 5...
+                            & trigs(ind)<FS
                         M=[M,  alls1( trigs(ind)-windS:trigs(ind)+windS,tr)];
-                        
                     end
                     
-                    if trigs(ind)-windS>0  & trigs(ind) +windS < size(alls1,1) & (trigs(ind+1)-trigs(ind))>5 & trigs(ind)>FS  &trigs(ind)<FS*2
+                    if trigs(ind)-windS>0  & trigs(ind) +windS < size(alls1,1) & (trigs(ind+1)-trigs(ind))>5...
+                            & trigs(ind)>FS  &trigs(ind)<FS*2
                         MS=[MS,  alls1( trigs(ind)-windS:trigs(ind)+windS,tr)];
                         VS=[VS,  allV( trigs(ind)-windS:trigs(ind)+windS,tr)];
                         MS1=[MS1,  alls1( trigs(ind)-windS:trigs(ind)+windS,tr)];
                         VS1=[VS1,  allV( trigs(ind)-windS:trigs(ind)+windS,tr)];
                         O=[O,  opto( trigs(ind)-windS:trigs(ind)+windS)'];
-                        
                     end
-                end;
+                end
             end
         end
         
         if ~isempty(MS1)
-            
             mj=mj+1;
-            
             allmod_S(:,mj)=nanmean(MS1,2);
-            allmod_V(:,mj)=nanmean(VS1,2);allmod_V(:,mj)=allmod_V(:,mj)-nanmean(allmod_V(:,mj));
-            
+            allmod_V(:,mj)=nanmean(VS1,2);
+            allmod_V(:,mj)=allmod_V(:,mj)-nanmean(allmod_V(:,mj));
         end
 
         allV2(:,mj)= nanmean(aV,2)./nanmean(asA);
-        allV2(:,mj)=allV2(:,mj)-nanmean(allV2(500:800,mj));
-        allses(mj)=     sesid;
-        
+        allV2(:,mj)= allV2(:,mj)-nanmean(allV2(500:800,mj));
+        allses(mj)= sesid;
     end
-    
 end
 
 savepath='\\engnas.bu.edu\research\eng_research_handata\EricLowet\DBS\revision\'
 pheight=160
 
+% 
 timax=([-windS:windS]-1).*1.2;
 figure('COlor','w','Position', [ 300 400 200 pheight],'Renderer', 'painters')
 plot(timax,(nanmean(MS,2)).*FS,'k');hold on,
 fill_error_area2(timax, nanmean(MS,2).*FS,   (nanstd(MS,[],2).*FS)./sqrt(size(MS,2)), [ 0.5 0.5 .5])
 if stim_freq==1 
-minM=0; maxM=80;
-   line([ 0 0],[ minM maxM],'Color',[ 0.3 0.3 0.3],'Linestyle','--')
+    minM=0; 
+    maxM=80;
+    line([ 0 0],[ minM maxM],'Color',[ 0.3 0.3 0.3],'Linestyle','--')
     line([ -25 -25],[ minM maxM],'Color',[ 0.3 0.3 0.3],'Linestyle','--')
     line([ 25 25],[ minM maxM],'Color',[ 0.3 0.3 0.3],'Linestyle','--')
     axis tight
