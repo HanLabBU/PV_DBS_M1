45e29ac96ba0453984a1b147a6eddfdc79738b52
Pier Fier <fabrispierre@gmail.com>
2023-03-19 23:23:08 -0400

    Calc stats counts number of neurons per mouse

--- calc_stats.m
+++ calc_stats.m
@@ -6,24 +6,25 @@ f = filesep;
 
 %%%---- USER Modification--------
 % Linux server
-server_root_path = '~/Projects/';
+local_root_path = '~/Projects/';
+server_root_path = '~/handata_server/eng_research_handata3/';
 
 % Windows server
-%server_root_path = 'Z:\';
-
+%local_root_path = 'Z:\';
 
 % Data on server
-%pv_data_path = [server_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'PV_Data' f];
+%pv_data_path = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'PV_Data' f];
 % Data on local linux machine
-pv_data_path = [server_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'PV_Data' f];
+%pv_data_path = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'PV_Data' f];
+pv_data_path = [server_root_path 'Pierre Fabris' f 'PV Project' f 'PV_Data' f];
 
 % Specify which ignore trials to use
-ignore_trial_dict = Multi_func.csv_to_struct([server_root_path 'Pierre Fabris' f 'PV DBS neocortex' f ...
+ignore_trial_dict = Multi_func.csv_to_struct([local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f ...
                                        'Recordings' f 'Data_Config' f 'byvis_ignore.csv']);
 %%%----- END Modification---------
 
 % Check that the server path exists
-if ~isfolder(server_root_path)
+if ~isfolder(local_root_path)
     disp('Server rootpath does not exist!!!');
     return;
 end
@@ -38,71 +39,78 @@ for f_region = fieldnames(region_matfiles)'
 
     % Create brain region struct
     region_stats.(f_region) = struct();
-    
-    %% Select matfiles by stim condition by this iteration's brain region
-    [matfile_stim] = stim_cond(region_matfiles.(f_region).names);
-    
-    %% Loop through each field of the struct and concatenate everything together
-    % Keep track the stats of the number of trials and neurons per mouse
-    cond_stats = struct();
-    % Loop through each stimulation condition
-    for field = fieldnames(matfile_stim)'
-        field = field{1};
-    
-        matfiles = matfile_stim.(field).names;    
-     
-        % Initialize field to keep track of neurons and trials
-        cond_stats.(field) = struct();
-        cond_stats.(field).trial_nums = [];
-        cond_stats.(field).num_fovs = 0;
-    
-    
-        % Loop through each matfile of the current stimulation condition
-        for matfile =matfiles
-            % Read in the mat file of the current condition
-            data = load([pv_data_path matfile{1}]);
-     
-            %TODO need to loop through ROIs
-    
-            %% OLD CODE
-            % Grab the number of trials to ignore from this neuron
-            %ri = strsplit(matfile{1}, '_');
-            %if isfield(ignore_trial_dict.(['mouse_' ri{1}]), (['rec_' erase(ri{3}, 'rec')])) && ...
-            %    isfield(ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]), (ri{4})) && ...
-            %    isfield(ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}), (['f_' ri{5}]))
-    
-            %    num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI1;
-            %else
-            %    num_ignore = 0;
-            %end
-    
-            %TODO I should just encapsulate this into a try/catch block and then catch with num_ignore = 0
-            ri = strsplit(matfile{1}, '_');
-            try 
-                % Try to grab ROI1's ignoring, if doesn't exist look at ROI2 
-                try
-                    num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI1;
+ 
+    % Grab all of the mice for this brain region
+    [mouse_matfiles] = find_mouse(region_matfiles.(f_region).names);
+    for f_mouse = fieldnames(mouse_matfiles)'
+        f_mouse = f_mouse{1};
+        
+        % Initialize mouse stats
+        mouse_stats.(f_mouse) = struct();
+        
+        %% Select matfiles by stim condition by this iteration's brain region
+        [matfile_stim] = find_stim_cond(mouse_matfiles.(f_mouse).names);
+        
+        %% Loop through each field of the struct and concatenate everything together
+        % Keep track the stats of the number of trials and neurons per mouse
+        cond_stats = struct();
+        % Loop through each stimulation condition
+        for field = fieldnames(matfile_stim)'
+            field = field{1};
+            matfiles = matfile_stim.(field).names;    
+         
+            % Initialize field to keep track of neurons and trials
+            cond_stats.(field) = struct();
+            cond_stats.(field).trial_nums = [];
+            cond_stats.(field).num_fovs = 0;
+       
+            % Loop through each matfile of the current stimulation condition
+            for matfile =matfiles
+                % Read in the mat file of the current condition
+                data = load([pv_data_path matfile{1}]);
+         
+                %TODO need to loop through ROIs
+        
+                %% OLD CODE
+                % Grab the number of trials to ignore from this neuron
+                %ri = strsplit(matfile{1}, '_');
+                %if isfield(ignore_trial_dict.(['mouse_' ri{1}]), (['rec_' erase(ri{3}, 'rec')])) && ...
+                %    isfield(ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]), (ri{4})) && ...
+                %    isfield(ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}), (['f_' ri{5}]))
+        
+                %    num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI1;
+                %else
+                %    num_ignore = 0;
+                %end
+        
+                %TODO I should just encapsulate this into a try/catch block and then catch with num_ignore = 0
+                ri = strsplit(matfile{1}, '_');
+                try 
+                    % Try to grab ROI1's ignoring trials array, if doesn't exist look at ROI2 
+                    try
+                        num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI1;
+                    catch
+                        num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI2;
+                    end
+                    
+                    num_ignore = length(num_ignore);
                 catch
-                    num_ignore = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).ROI2;
+                    num_ignore = 0;
                 end
+        
+                % Add the number of trials for this specific neuron and check if at least one trial is included in calculation
+                cond_stats.(field).trial_nums(end + 1) = length(data.align.trial) - num_ignore;
                 
-                num_ignore = length(num_ignore);
-            catch
-                num_ignore = 0;
-            end
-    
-            % Add the number of trials for this specific neuron and check if at least one trial is included in calculation
-            cond_stats.(field).trial_nums(end + 1) = length(data.align.trial) - num_ignore;
-            
-            if length(data.align.trial) - num_ignore > 2
-                cond_stats.(field).num_fovs = cond_stats.(field).num_fovs + 1;
-            end
-    
-        end % End looping through FOVs of a condition
+                % Count the neuron if it has more than 1 trial
+                if length(data.align.trial) - num_ignore > 1
+                    cond_stats.(field).num_fovs(end + 1) = str2num(erase(ri{4}, 'FOV'));
+                    %erase(ri{4}, 'FOV')
+                end
+        
+            end % End looping through FOVs of a condition
+        end % End looping through all of the mice 
+        region_stats.(f_region).(f_mouse).cond_stats = cond_stats;
     end
-
-    % Save the stim conditions for this iteration's brain region
-    region_stats.(f_region).cond_stats = cond_stats;
 end
 
 % Loop through and show each regions conditions
@@ -111,20 +119,28 @@ for f_region = regions'
     f_region = f_region{1};
     disp(f_region);
 
-    stims = fieldnames(region_stats.(f_region).cond_stats);
-    cond_stats = region_stats.(f_region).cond_stats;
-    for stim=stims'
-        stim = stim{1};
-        disp(stim);
-        disp(['Average trial per neuron ' num2str(nanmean(cond_stats.(stim).trial_nums)) ]);
-        disp(['Number of neurons ' num2str(cond_stats.(stim).num_fovs) ]);
-    end
+    % Loop through each mouse
+    mice = fieldnames(region_stats.(f_region));
+    for f_mouse = mice'
+        f_mouse = f_mouse{1};
+        disp(f_mouse);
 
-    fprintf('\n\n');
+        stims = fieldnames(region_stats.(f_region).(f_mouse).cond_stats);
+        cond_stats = region_stats.(f_region).(f_mouse).cond_stats;
+        for stim=stims'
+            stim = stim{1};
+            disp(stim);
+            disp(['Average trial per neuron ' num2str(nanmean(cond_stats.(stim).trial_nums)) ]);
+            disp(['Number of neurons ' num2str(numel(cond_stats.(stim).num_fovs)) ]);
+            disp(['FOVs are ' num2str(cond_stats.(stim).num_fovs)]);
+        end
+
+        fprintf('\n\n');
+    end
 end
 
 % Return matfiles by stimulation condition
-function [cond_struct] = stim_cond(matfile_names)
+function [cond_struct] = find_stim_cond(matfile_names)
     cond_struct = struct();
     
     % Loop through each matfilename and group by stimulation conditions
@@ -156,5 +172,22 @@ function [region_struct] = find_region(matfile_names)
         end
 
         region_struct.(['r_' reg]).names{end+1} = matfile_names{i};
+    end
+end
+
+% Find matfiles by mouse cage number
+function [mouse_struct] = find_mouse(matfile_names)
+    mouse_struct = struct();
+
+    for i=1:length(matfile_names)
+        file_parts = split(matfile_names{i}, '_');
+        mouse = file_parts{1};
+        
+        % Create mouse field if it does not exist
+        if ~isfield(mouse_struct, ['m_' mouse])
+            mouse_struct.(['m_' mouse]).names = {};
+        end
+
+        mouse_struct.(['m_' mouse]).names{end+1} = matfile_names{i};
     end
 end
