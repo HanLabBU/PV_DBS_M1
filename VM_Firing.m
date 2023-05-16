0ce8217dedc1dbf552c317d25679aa45d23b43e1
Pier Fier <fabrispierre@gmail.com>
2023-05-16 11:50:28 -0400

    Set neuron criteria to more than 2 trials and cleaned up some syntax

--- VM_Firing.m
+++ VM_Firing.m
@@ -105,28 +105,26 @@ for f_region = fieldnames(region_matfiles)'
 
             % Loop through each ROI
             for roi_idx=1:size(trial_data.detrend_traces, 2)
+                %Determine whether this roi is to be ignored for this particular trial
+                ri = strsplit(matfile{1}, '_');
+                try
+                    trial_ignr_list = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).(['ROI' num2str(roi_idx)]);
+                catch
+                    trial_ignr_list = [];
+                end
+
+                % Remove trials from trial idx list
+                trial_idxs = setdiff(trial_idxs, trial_ignr_list);
+
+                % Skip ROI if there are at most 2 trials
+                if length(trial_idxs) <= 2
+                    continue;
+                end
+
                 % Loop through each trial                
                 for tr_idx=trial_idxs        
                     trial_data = data.align.trial{tr_idx};
                     raw_trial_data = data.raw.trial{tr_idx};
-
-                    %Determine whether this roi is to be ignored for this particular trial
-                    ri = strsplit(matfile{1}, '_');
-                    try
-                        trial_ignr_list = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).(['ROI' num2str(roi_idx)]);
-                    catch
-                        trial_ignr_list = [];
-                    end
-    
-                    % Check if current trial is in the ignore list
-                    if ismember(tr_idx, trial_ignr_list)
-                        continue;
-                    end
-                    
-                    % If the trial data is empty, that means it was skipped
-                    if isempty(trial_data) || sum(isnan(cur_fov_subVm(:))) > 0
-                        continue;
-                    end
 
                     % Store the camera framerate
                     all_Fs(end+1) = trial_data.camera_framerate;
@@ -152,6 +150,8 @@ for f_region = fieldnames(region_matfiles)'
                     cur_trace_ws = trial_data.spike_info375.trace_ws(roi_idx, front_frame_drop:back_frame_drop);
                     [baseline, coeff] = Multi_func.exp_fit_Fx(cur_trace_ws', round(trial_data.camera_framerate));
                     detrend_subVm = cur_trace_ws - baseline;
+                    % This normalization did not seem to change the plot much
+                    %detrend_subVm = detrend_subVm./mean(trial_data.spike_info375.spike_amplitude{1}, 'omitnan');
                     cur_fov_subVm = horzcat_pad(cur_fov_subVm, detrend_subVm');
                     
                     % Calculate the spike rate
@@ -232,11 +232,12 @@ for f_region = fieldnames(region_data)'
     figure('visible', 'off', 'Renderer', 'Painters', 'Position', [200 200 2000 700]);
     tiledlayout(1, length(stims), 'TileSpacing', 'compact', 'Padding', 'compact');
     for f_stim=stims'
-        timeline = nanmean(data_bystim.(f_stim{1}).trace_timestamps, 2);
-        cur_srate = mean(data_bystim.(f_stim{1}).neuron_srate, 2, 'omitnan');
-        std_srate = std(data_bystim.(f_stim{1}).neuron_srate, 0, 2, 'omitnan');
-        num_neurons = size(data_bystim.(f_stim{1}).neuron_srate, 2);
-        %num_points = size(data_bystim.(f_stim{1}).neuron_srate, 1);
+        f_stim = f_stim{1};
+        timeline = nanmean(data_bystim.(f_stim).trace_timestamps, 2);
+        cur_srate = mean(data_bystim.(f_stim).neuron_srate, 2, 'omitnan');
+        std_srate = std(data_bystim.(f_stim).neuron_srate, 0, 2, 'omitnan');
+        num_neurons = size(data_bystim.(f_stim).neuron_srate, 2);
+        %num_points = size(data_bystim.(f_stim).neuron_srate, 1);
         sem_srate = std_srate./sqrt(num_neurons);
         nexttile;
         f = fill([timeline; flip(timeline)], [cur_srate + sem_srate; flipud(cur_srate - sem_srate)], [0.5 0.5 0.5]);
@@ -246,22 +247,20 @@ for f_region = fieldnames(region_data)'
         hold on;
 
         % Plot DBS pulse bar
-        Multi_func.plot_dbs_bar([0, 1], max(cur_srate + sem_srate), [f_stim{1}(3:end) 'Hz DBS']);
-
-        set(gca, 'color', 'none');
+        Multi_func.plot_dbs_bar([0, 1], max(cur_srate + sem_srate), [f_stim(3:end) 'Hz DBS']);
         xlabel('Time from stim onset (S)');
+        xlim([-1 2.05]);
         ylabel('Firing Rate (Hz)');
-        yyaxis right;
-        yticks([]);
-
-        title(f_stim{1}(3:end), 'Interpreter', 'none');
+        x = gca; x = x.YAxis;
+        Multi_func.set_spacing_axis(x, 4, 1);
+        Multi_func.set_default_axis(gca);
+        title(f_stim(3:end), 'Interpreter', 'none');
     end
     sgtitle([f_region ' Average Spike rate'], 'Interpreter', 'none');
     saveas(gcf, [figure_path 'Average/' f_region '_Summary_Continuous_FiringRate.png']);
+    saveas(gcf, [figure_path 'Average/' f_region '_Summary_Continuous_FiringRate.pdf']);
     saveas(gcf, [figure_path 'Average/' f_region '_Summary_Continuous_FiringRate.eps']);
 end
-
-
 
 %% Subthreshold Vm
 for f_region = fieldnames(region_data)'
@@ -272,10 +271,11 @@ for f_region = fieldnames(region_data)'
     figure('visible', 'off', 'Renderer', 'Painters', 'Position', [200 200 2000 700]);
     tiledlayout(1, length(stims), 'TileSpacing', 'compact', 'Padding', 'compact');
     for f_stim=stims'
-        timeline = nanmean(data_bystim.(f_stim{1}).trace_timestamps, 2);
-        cur_Vm = mean(data_bystim.(f_stim{1}).neuron_Vm, 2, 'omitnan');
-        std_Vm = std(data_bystim.(f_stim{1}).neuron_Vm, 0, 2, 'omitnan');
-        num_neurons = size(data_bystim.(f_stim{1}).neuron_Vm, 2);
+        f_stim = f_stim{1};
+        timeline = nanmean(data_bystim.(f_stim).trace_timestamps, 2);
+        cur_Vm = mean(data_bystim.(f_stim).neuron_Vm, 2, 'omitnan');
+        std_Vm = std(data_bystim.(f_stim).neuron_Vm, 0, 2, 'omitnan');
+        num_neurons = size(data_bystim.(f_stim).neuron_Vm, 2);
         sem_Vm = std_Vm./sqrt(num_neurons);
         %num_points = size(data_bystim.(f_stim{1}).neuron_Vm, 1);
         nexttile;
@@ -286,17 +286,20 @@ for f_region = fieldnames(region_data)'
         hold on;
 
         % Plot DBS pulse bar
-        Multi_func.plot_dbs_bar([0, 1], max(cur_Vm + sem_Vm), [f_stim{1}(3:end) 'Hz DBS']);
+        Multi_func.plot_dbs_bar([0, 1], max(cur_Vm + sem_Vm), [f_stim(3:end) 'Hz DBS']);
 
-        title(f_stim{1}(3:end), 'Interpreter', 'none');
+        title(f_stim(3:end), 'Interpreter', 'none');
         xlabel('Time from stim onset (S)');
+        xlim([-1 2.05]);
         ylabel('Raw Vm (A.U.)');
         set(gca, 'color', 'none');
-        yyaxis right;
-        yticks([]);
+        Multi_func.set_default_axis(gca);
+        x = gca; x = x.YAxis;
+        Multi_func.set_spacing_axis(x, 4, 1);
     end
     sgtitle([f_region ' Average subthreshold Vm'], 'Interpreter', 'none');
     saveas(gcf, [figure_path 'Average/' f_region '_Average_sub_thres.png']);
+    saveas(gcf, [figure_path 'Average/' f_region '_Average_sub_thres.pdf']);
     saveas(gcf, [figure_path 'Average/' f_region '_Average_sub_thres.eps']);
 end
 
@@ -309,7 +312,7 @@ for f_region = fieldnames(region_data)'
     % Struct to identify each group of data
     sub_vm_stat_data = struct();
 
-    figure('Renderer', 'Painters', 'Position', [200 200 900 400]);
+    figure('visible', 'off', 'Renderer', 'Painters', 'Position', [200 200 900 400]);
     tiledlayout(1, length(stims), 'TileSpacing', 'compact', 'Padding', 'compact');
     for f_stim=stims'
         nexttile;
@@ -340,29 +343,55 @@ for f_region = fieldnames(region_data)'
     saveas(gcf, [figure_path 'Average/' f_region '_ped_comp_sub_thres.png']);
     saveas(gcf, [figure_path 'Average/' f_region '_ped_comp_sub_thres.eps']);
 end
+
+% transient compared to baseline statistical test 
+[p, h, stats] = signtest(sub_vm_stat_data.f_40.trans_vm)
+SubVm_40_trans_stats = struct();
+SubVm_40_trans_stats.p = p;
+SubVm_40_trans_stats.h = h;
+SubVm_40_trans_stats.stats = stats;
+
+[p, h, stats] = signtest(sub_vm_stat_data.f_140.trans_vm)
+SubVm_140_trans_stats = struct();
+SubVm_140_trans_stats.p = p;
+SubVm_140_trans_stats.h = h;
+SubVm_140_trans_stats.stats = stats;
+
+% transient compared to baseline statistical test 
+[p, h, stats] = signtest(sub_vm_stat_data.f_40.sus_vm)
+SubVm_40_sus_stats = struct();
+SubVm_40_sus_stats.p = p;
+SubVm_40_sus_stats.h = h;
+SubVm_40_sus_stats.stats = stats;
+
+[p, h, stats] = signtest(sub_vm_stat_data.f_140.sus_vm)
+SubVm_140_sus_stats = struct();
+SubVm_140_sus_stats.p = p;
+SubVm_140_sus_stats.h = h;
+SubVm_140_sus_stats.stats = stats;
+
 % Trans and Sus SubVm statistical test
-[p, h, stats] = signrank(sub_vm_stat_data.f_40.trans_vm, sub_vm_stat_data.f_40.sus_vm);
+[p, h, stats] = signrank(sub_vm_stat_data.f_40.trans_vm, sub_vm_stat_data.f_40.sus_vm)
 signrank_SubVm_40_trans_sus_stats = struct();
 signrank_SubVm_40_trans_sus_stats.p = p
 signrank_SubVm_40_trans_sus_stats.h = h;
 signrank_SubVm_40_trans_sus_stats.stats = stats;
 
-[p, h, stats] = signrank(sub_vm_stat_data.f_140.trans_vm, sub_vm_stat_data.f_140.sus_vm);
+[p, h, stats] = signrank(sub_vm_stat_data.f_140.trans_vm, sub_vm_stat_data.f_140.sus_vm)
 signrank_Subvm_140_trans_sus_stats = struct();
 signrank_Subvm_140_trans_sus_stats.p = p
 signrank_Subvm_140_trans_sus_stats.h = h;
 signrank_Subvm_140_trans_sus_stats.stats = stats;
 
-
 % Trans to Trans Subvm statistical test
-[p, h, stats] = ranksum(sub_vm_stat_data.f_40.trans_vm, sub_vm_stat_data.f_140.trans_vm);
+[p, h, stats] = ranksum(sub_vm_stat_data.f_40.trans_vm, sub_vm_stat_data.f_140.trans_vm)
 ranksum_Subvm_trans_trans_stats = struct();
 ranksum_Subvm_trans_trans_stats.p = p
 ranksum_Subvm_trans_trans_stats.h = h;
 ranksum_Subvm_trans_trans_stats.stats = stats;
 
 % Sus to sus Subvm statistical test
-[p, h, stats] = ranksum(sub_vm_stat_data.f_40.sus_vm, sub_vm_stat_data.f_140.sus_vm);
+[p, h, stats] = ranksum(sub_vm_stat_data.f_40.sus_vm, sub_vm_stat_data.f_140.sus_vm)
 ranksum_Subvm_sus_sus_stats = struct();
 ranksum_Subvm_sus_sus_stats.p = p
 ranksum_Subvm_sus_sus_stats.h = h;
