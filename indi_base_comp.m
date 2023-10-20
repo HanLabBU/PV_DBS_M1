3619dc772e907bc7eb7aa73166709236ad64fe20
Pier Fier <fabrispierre@gmail.com>
2023-10-20 16:07:34 -0400

    Changed 'indi_base_camp' to plot each Vm in different y axes

--- indi_base_comp.m
+++ indi_base_comp.m
@@ -22,7 +22,8 @@ back_frame_drop = 2496;
 % Data on handata3 folder
 pv_data_path = [server_root_path 'Pierre Fabris' f 'PV Project' f 'PV_Data' f];
 
-figure_path = [server_root_path 'Pierre Fabris' f 'PV Project' f 'Plots' f];
+%figure_path = [server_root_path 'Pierre Fabris' f 'PV Project' f 'Plots' f];
+figure_path = Multi_func.save_plot;
 
 % CSV file to determine which trials to ignore
 ignore_trial_dict = Multi_func.csv_to_struct([local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f ...
@@ -30,10 +31,16 @@ ignore_trial_dict = Multi_func.csv_to_struct([local_root_path 'Pierre Fabris' f 
 
 % Smoothing parameter for spike rate
 % Using a smaller window size
-srate_win = 20;
+srate_win = 3;
 
 % Do all region
-all_region = 1;
+all_region = 0;
+
+% Percentage to increase Vm and firing rate
+thres_percent = 0.10;
+
+% Set threshold higher
+
 %%% END Modification
 
 % Check that the server path exists
@@ -43,7 +50,7 @@ if ~isfolder(local_root_path)
 end
 
 % Load all of the data
-save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'pv_data.mat'];
+save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'pv_data_ex200.mat'];
 load(save_all_data_file);
 
 % Combine all regions into 1
@@ -51,61 +58,85 @@ if all_region == 1
     region_data = Multi_func.combine_regions(region_data);
 end
 
-avg_Fs = mean(region_data.r_combine.f_40.framerate, 'omitnan');
+field1 = fieldnames(region_data);
+field1 = field1{1};
+avg_Fs = mean(region_data.(field1).f_40.framerate, 'omitnan');
 timeline = ( (4+(front_frame_drop:back_frame_drop) )./avg_Fs) - 1;
 
 %set figures off
-%set(0,'DefaultFigureVisible','off');
+set(0,'DefaultFigureVisible','off');
 
 spacing = 1.4;
 
-% Show all of the neuron's trial-averaged Vm
+% Show each neuron's trial-averaged Vm
 for f_region = fieldnames(region_data)'    
     f_region = f_region{1};    
     data_bystim = region_data.(f_region);    
     stims = fieldnames(data_bystim);    
-        
-    figure('Renderer', 'Painters', 'Position', [200 200 2000 1000]);    
-    tiledlayout(1, length(stims), 'TileSpacing', 'compact', 'Padding', 'compact');    
     
     for f_stim=stims'
+        
         f_stim = f_stim{1};
-     
-        nexttile;
+        figure('Renderer', 'Painters', 'Position', [200 200 2000 1000]);    
+        tiledlayout(size(data_bystim.(f_stim).trace_timestamps, 2), 1, 'TileSpacing', 'none', 'Padding', 'compact');    
+
+        
         timeline = nanmean(data_bystim.(f_stim).trace_timestamps, 2); 
-        % Plot each neuron's average Vm
-        neuron_vm = data_bystim.(f_stim).neuron_Vm;
-        neuron_vm = (neuron_vm - min(neuron_vm, [], 1))./ (max(neuron_vm, [], 1) - min(neuron_vm, [], 1));
-        plot(timeline, neuron_vm + [spacing:spacing:size(neuron_vm, 2)*spacing], '-k');
-        hold on;
 
-        %-- Find points with higher than 3 std baseline
-        base_vm = [];    
-        % Grab the baseline sub Vm for all neurons    
-        for i = 1:size(data_bystim.(f_stim).trace_timestamps, 2)    
-            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, i) < data_bystim.(f_stim).stim_timestamps(1, i));
-            base_vm = horzcat_pad(base_vm, neuron_vm(baseline_idx, i) );
-        end 
-        
-        % Calculate the baseline std for each neuron
-        avg_base_vm = mean(base_vm, 1, 'omitnan');
-        base_std = std(base_vm, 0, 1, 'omitnan');
+        % Loop through each neuron
+        for nr=1:size(data_bystim.(f_stim).trace_timestamps, 2)
+            nexttile;
+            % Plot each neuron's average Vm
+            neuron_vm = data_bystim.(f_stim).neuron_Vm(:, nr);
+            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, nr) < data_bystim.(f_stim).stim_timestamps(1, nr));
+            stim_idx = find(data_bystim.(f_stim).trace_timestamps(:, nr) >= data_bystim.(f_stim).stim_timestamps(1, nr) & data_bystim.(f_stim).trace_timestamps(:, nr) <= data_bystim.(f_stim).stim_timestamps(end, nr));
 
-        [row, col] = find(neuron_vm > 3*base_std + avg_base_vm);
-        
-        plot(timeline(row), spacing*.75 + col*spacing, '.b', 'MarkerSize', 6);
-        xlim([-1 2.05]);
-        Multi_func.set_default_axis(gca);
-        
-        title(f_stim, 'Interpreter', 'none');
+            % Plotting all neuron's Vm normalized
+            %neuron_vm = (neuron_vm - min(neuron_vm, [], 1))./ (max(neuron_vm, [], 1) - min(neuron_vm, [], 1));
+            %plot(timeline, neuron_vm + [spacing:spacing:size(neuron_vm, 2)*spacing], '-k');
+            base_vm = mean(neuron_vm(baseline_idx), 'omitnan');
+            stim_vm = mean(neuron_vm(stim_idx), 'omitnan');
+
+            % Check for Vm increase
+            color = [0 0 0 0.4];
+            if stim_vm > base_vm*(1 + thres_percent)
+                color = [[30, 2, 237]/255, 0.4];
+            elseif stim_vm < base_vm*(1 - thres_percent)
+                color = [[235, 5, 28]/255, 0.4];
+            end
+            plot(timeline, neuron_vm, 'Color', color);
+            hold on;
+
+            %-- Find points with higher than 2 std baseline
+            
+            % Calculate the baseline std for each neuron
+            avg_base_vm = mean(base_vm, 1, 'omitnan');
+            base_std = std(base_vm, 0, 1, 'omitnan');
+
+            [row, col] = find(neuron_vm > 2*base_std + avg_base_vm);
+            
+            plot(timeline(row), max(neuron_vm) + 1, '.b', 'MarkerSize', 6);
+            hold on;
+            xlim([-1 2.05]);
+            ylim([min(neuron_vm) - 1, max(neuron_vm) + 1]);
+            set(gca,'XTick',[]);
+            Multi_func.set_default_axis(gca);
+            
+            % Plot the DBS onset line
+            xline(0, '--');
+            hold on;
+            xline(1, '--');
+
+        end
+
+        sgtitle([f_region(3:end) '_' f_stim(3:end) ' Vm with 2 stds above baseline for individual neurons' ], 'Interpreter', 'non');
+
+        % Save the figure
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_vm_base_comp.png']);
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_vm_base_comp.pdf']);
+        %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_vm_base_comp.eps'], 'epsc');
     end
-
-    % Save the figure
-    saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_vm_base_comp.png']);
-    %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_vm_base_comp.pdf']);
-    %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_vm_base_comp.eps'], 'epsc');
 end
-
 
 % Show all of the neuron's trial-averaged Firing Rate
 for f_region = fieldnames(region_data)'    
@@ -113,46 +144,125 @@ for f_region = fieldnames(region_data)'
     data_bystim = region_data.(f_region);    
     stims = fieldnames(data_bystim);    
         
-    figure('Renderer', 'Painters', 'Position', [200 200 2000 1000]);    
-    tiledlayout(1, length(stims), 'TileSpacing', 'compact', 'Padding', 'compact');    
+    for f_stim=stims'
+        f_stim = f_stim{1};
+            
+        figure('Renderer', 'Painters', 'Position', [200 200 2000 1000]);    
+        tiledlayout(size(data_bystim.(f_stim).trace_timestamps, 2), 1, 'TileSpacing', 'none', 'Padding', 'compact');    
+        timeline = nanmean(data_bystim.(f_stim).trace_timestamps, 2); 
+        
+        for nr=1:size(data_bystim.(f_stim).trace_timestamps, 2)
+            nexttile;
+            % Plot each neuron's average Firing Rate
+            neuron_fr = data_bystim.(f_stim).neuron_srate_50(:, nr); 
+            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, nr) < data_bystim.(f_stim).stim_timestamps(1, nr));
+            stim_idx = find(data_bystim.(f_stim).trace_timestamps(:, nr) >= data_bystim.(f_stim).stim_timestamps(1, nr) & data_bystim.(f_stim).trace_timestamps(:, nr) <= data_bystim.(f_stim).stim_timestamps(end, nr));
+
+            % Plotting normalized firing rate
+            %neuron_fr = (neuron_fr - min(neuron_fr, [], 1))./ (max(neuron_fr, [], 1) - min(neuron_fr, [], 1));
+            %plot(timeline, neuron_fr + [spacing:spacing:size(neuron_fr, 2)*spacing], '-k');
+            base_fr = mean(neuron_fr(baseline_idx), 'omitnan');
+            stim_fr = mean(neuron_fr(stim_idx), 'omitnan');
+            
+            % Plot the firing rate with color indicating increase or decrease
+            color = [0 0 0 0.4];
+            if stim_fr > base_fr*(1 + thres_percent)
+                color = [[30, 2, 237]/255, 0.4];
+            elseif stim_fr < base_fr*(1 - thres_percent)
+                color = [[235, 5, 28]/255, 0.4];
+            end
+            plot(timeline, neuron_fr, 'Color', color);
+            hold on;
+
+            %-- Find points with higher than 2 std baseline
+            % Calculate the baseline std for each neuron
+            avg_base_fr = mean(base_fr, 1, 'omitnan');
+            base_std = std(base_fr, 0, 1, 'omitnan');
+
+            [row, col] = find(neuron_fr > 2*base_std + avg_base_fr);
+            
+            plot(timeline(row), max(neuron_fr) + 1, '.b', 'MarkerSize', 6);
+            hold on;
+            xlim([-1 2.05]);
+            ylim([min(neuron_fr) - 1, max(neuron_fr) + 1]);
+            set(gca,'XTick',[]);
+            Multi_func.set_default_axis(gca);
+
+            % Plot the DBS onset line
+            xline(0, '--');
+            hold on;
+            xline(1, '--');
+        end
+
+        sgtitle([f_region(3:end) ' ' f_stim(3:end) ' Firing Rate with 2 stds above baseline for individual neurons' ], 'Interpreter', 'none');
+
+        % Save the figure
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_fr_base_comp.png']);
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_fr_base_comp.pdf']);
+        %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_fr_base_comp.eps'], 'epsc');
+    end
+end
+
+% Plot each neuron's Vm between baseline and stimulation
+for f_region = fieldnames(region_data)'    
+    f_region = f_region{1};    
+    data_bystim = region_data.(f_region);    
+    stims = fieldnames(data_bystim);    
     
     for f_stim=stims'
         f_stim = f_stim{1};
-     
-        nexttile;
-        timeline = nanmean(data_bystim.(f_stim).trace_timestamps, 2); 
-        % Plot each neuron's average Firing Rate
-        neuron_fr = data_bystim.(f_stim).neuron_srate_large; %TODO change this
-        neuron_fr = (neuron_fr - min(neuron_fr, [], 1))./ (max(neuron_fr, [], 1) - min(neuron_fr, [], 1));
-        plot(timeline, neuron_fr + [spacing:spacing:size(neuron_fr, 2)*spacing], '-k');
+        base_vm = [];
+        stim_vm = [];
+
+        for neuron = 1:size(data_bystim.(f_stim).trace_timestamps, 2)
+            timeline = data_bystim.(f_stim).trace_timestamps(:, neuron); 
+            
+            % Find the region idxs
+            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, neuron) < data_bystim.(f_stim).stim_timestamps(1, neuron));
+            stim_idx = find(data_bystim.(f_stim).trace_timestamps(:, neuron) >= data_bystim.(f_stim).stim_timestamps(1, neuron) & data_bystim.(f_stim).trace_timestamps(:, neuron) <= data_bystim.(f_stim).stim_timestamps(end, neuron));
+
+            neuron_vm = data_bystim.(f_stim).neuron_Vm(:, neuron);
+           
+            base_vm(end + 1) = mean(neuron_vm(baseline_idx), 'omitnan');
+            stim_vm(end + 1) = mean(neuron_vm(stim_idx), 'omitnan');
+        end
+
+        % PLot the violins of the data
+        figure('visible', 'on', 'Renderer', 'Painters', 'Units', 'centimeters', 'Position', [4 20 21.59 27.94]);
+        data = [base_vm, stim_vm];
+        %data = log2(data);
+        labels = [repmat({'Base'}, 1, length(base_vm)), repmat({'Stim'}, 1, length(stim_vm))];
+        ViolinOpts = Multi_func.get_default_violin();
+        violins = violinplot(data, labels, 'GroupOrder', {'Base', 'Stim'}, ViolinOpts);
+        
+        violins(1).ViolinColor = {Multi_func.base_color};
+        violins(2).ViolinColor = {Multi_func.stim_color};
         hold on;
-
-        %-- Find points with higher than 3 std baseline
-        base_fr = [];    
-        % Grab the baseline Firing rate for all neurons    
-        for i = 1:size(data_bystim.(f_stim).trace_timestamps, 2)    
-            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, i) < data_bystim.(f_stim).stim_timestamps(1, i));
-            base_fr = horzcat_pad(base_fr, neuron_fr(baseline_idx, i) );
-        end 
         
-        % Calculate the baseline std for each neuron
-        avg_base_fr = mean(base_fr, 1, 'omitnan');
-        base_std = std(base_fr, 0, 1, 'omitnan');
-
-        [row, col] = find(neuron_fr > 3*base_std + avg_base_fr);
-        
-        plot(timeline(row), spacing*.75 + col*spacing, '.b', 'MarkerSize', 6);
-        xlim([-1 2.05]);
         Multi_func.set_default_axis(gca);
+        set(gca, 'Units', 'centimeters', 'InnerPosition', [1, 1, 16.2/2, 20]);
+        % Conditionally color lines between violinplots
+        for i=1:length(base_vm)
+            color = [0 0 0 0.4];
+            if stim_vm(i) > base_vm(i)*(1 + thres_percent)
+                color = [[30, 2, 237]/255, 0.4];
+            elseif stim_vm(i) < base_vm(i)*(1 - thres_percent)
+                color = [[235, 5, 28]/255, 0.4];
+            end
         
-        title(f_stim, 'Interpreter', 'none');
-    end
+            plot([1 2], ([base_vm(i), stim_vm(i)]), '-', 'Color', color);
+            hold on;
+        end
+        ylabel('Vm (A.U.)');
 
-    % Save the figure
-    saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_fr_base_comp.png']);
-    %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_fr_base_comp.pdf']);
-    %saveas(gcf, [figure_path 'Neuronwise/' f_region '_indi_fr_base_comp.eps'], 'epsc');
+        %Multi_func.set_default_axis(gca);
+        title([f_region(3:end) ' Vm for ' f_stim(3:end)]);
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_vm_base_comp_violin.png']);
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_vm_base_comp_violin.eps'], 'epsc');
+        saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_vm_base_comp_violin.pdf']);
+    end
 end
+
 
 % Plot each neuron's firing rate between baseline and stimulation
 for f_region = fieldnames(region_data)'    
@@ -169,12 +279,12 @@ for f_region = fieldnames(region_data)'
             timeline = data_bystim.(f_stim).trace_timestamps(:, neuron); 
             
             % Find the region idxs
-            base_idx = find(timeline < 0);
-            stim_idx = find(timeline >= 0 & timeline <= 1);
+            baseline_idx = find(data_bystim.(f_stim).trace_timestamps(:, neuron) < data_bystim.(f_stim).stim_timestamps(1, neuron));
+            stim_idx = find(data_bystim.(f_stim).trace_timestamps(:, neuron) >= data_bystim.(f_stim).stim_timestamps(1, neuron) & data_bystim.(f_stim).trace_timestamps(:, neuron) <= data_bystim.(f_stim).stim_timestamps(end, neuron));
 
             cur_nr = data_bystim.(f_stim).all_trial_spikeidx{neuron};
             
-            base_raster = ismember(cur_nr, base_idx);
+            base_raster = ismember(cur_nr, baseline_idx);
             stim_raster = ismember(cur_nr, stim_idx);
 
             % Calculate the spiking rate for 1 sec
@@ -186,7 +296,7 @@ for f_region = fieldnames(region_data)'
         end
 
         % PLot the violins of the data
-        figure('Renderer', 'Painters', 'Units', 'centimeters', 'Position', [4 20 21.59 27.94]);
+        figure('visible', 'on', 'Renderer', 'Painters', 'Units', 'centimeters', 'Position', [4 20 21.59 27.94]);
         data = [base_fr, stim_fr];
         %data = log2(data);
         labels = [repmat({'Base'}, 1, length(base_fr)), repmat({'Stim'}, 1, length(stim_fr))];
@@ -202,9 +312,9 @@ for f_region = fieldnames(region_data)'
         % Conditionally color lines between violinplots
         for i=1:length(base_fr)
             color = [0 0 0 0.4];
-            if stim_fr(i) > base_fr(i)*1.10
+            if stim_fr(i) > base_fr(i)*(1 + thres_percent)
                 color = [[30, 2, 237]/255, 0.4];
-            elseif stim_fr(i) < base_fr(i)*0.9
+            elseif stim_fr(i) < base_fr(i)*(1 - thres_percent)
                 color = [[235, 5, 28]/255, 0.4];
             end
         
@@ -214,7 +324,7 @@ for f_region = fieldnames(region_data)'
         ylabel('Firing Rate (Hz)');
 
         %Multi_func.set_default_axis(gca);
-        title(['Firing Rate for ' f_stim(3:end)]);
+        title([f_region(3:end) ' Firing Rate for ' f_stim(3:end)]);
         saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_fr_base_comp_violin.png']);
         saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_fr_base_comp_violin.eps'], 'epsc');
         saveas(gcf, [figure_path 'Neuronwise/' f_region '_' f_stim '_indi_fr_base_comp_violin.pdf']);
