7f5eead199a901a1151df0dd8428597108f89a3f
Pier Fier <fabrispierre@gmail.com>
2024-06-18 13:59:17 -0400

    Modified 'inter_trial_coh.m' to include selecting neurons based on baseline low frequencies

--- inter_trial_coh.m
+++ inter_trial_coh.m
@@ -30,188 +30,9 @@ ignore_trial_dict = Multi_func.csv_to_struct([local_root_path 'Pierre Fabris' f 
 % Parameter to determine whether to combine all regions as one data
 all_regions = 0;
 
-%%% END Modification
-
-% Check that the server path exists
-if ~isfolder(local_root_path)
-    disp('Server rootpath does not exist!!!');
-    return;
-end
-
-%ses = dir([pv_data_path '*.mat']);
-%
-%all_matfiles = {ses.name};
-%
-%% Select matfiles by brain region
-%[region_matfiles] = Multi_func.find_region(all_matfiles);
-%region_data = struct();
-%all_Fs = [];
-%for f_region = {'r_M1'} %fieldnames(region_matfiles)'
-%    f_region = f_region{1};
-%
-%    %% Select matfiles by stim specific conditions for all regions
-%    %[matfile_stim] = stim_cond(all_matfiles); 
-%    %% Select matfiles by stim condition for given region
-%    [matfile_stim] = stim_cond(region_matfiles.(f_region).names);
-%
-%    %% Loop through each field of the struct and concatenate everything together
-%    % Store trace aspect data by each individual stimulation condition
-%    data_bystim = struct();
-%    % Store all of the calculated sampling frequencies
-%
-%    % Loop through 40Hz stimulated neurons
-%    for f_stim = {'f_40'}
-%        f_stim = f_stim{1};
-%        matfiles = matfile_stim.(f_stim).names;    
-%    
-%        % Initialize field subthreshold array
-%        data_bystim.(f_stim) = struct();
-%        data_bystim.(f_stim).neuron_Vm = [];
-%        data_bystim.(f_stim).neuron_spec_power = [];
-%        data_bystim.(f_stim).neuron_spec_freq = [];
-%        data_bystim.(f_stim).stim_timestamps = [];
-%        data_bystim.(f_stim).trace_timestamps = [];
-%
-%        % Loop through each matfile of the current stimulation condition
-%        for matfile = matfiles(17) % DEBUG just trying the one neuron first
-%            % Read in the mat file of the current condition
-%            data = load([pv_data_path matfile{1}]);
-%            trial_idxs = find(~cellfun(@isempty, data.align.trial));
-%            trial_data = data.align.trial{trial_idxs(1)};    
-%            cur_fov_Fs = [];
-%            cur_fov_subVm = [];
-%            cur_fov_phases = [];
-%            cur_fov_fourcoeff = [];
-%            cur_fov_stim_time = [];
-%            cur_fov_trace_time = [];
-%            cur_fov_filtsig = [];
-%
-%            % Loop through each ROI
-%            for roi_idx=1:size(trial_data.detrend_traces, 2)
-%                %Determine whether this roi is to be ignored for this particular trial
-%                ri = strsplit(matfile{1}, '_');
-%                try
-%                    trial_ignr_list = ignore_trial_dict.(['mouse_' ri{1}]).(['rec_' erase(ri{3}, 'rec')]).(ri{4}).(['f_' ri{5}]).(['ROI' num2str(roi_idx)]);
-%                catch
-%                    trial_ignr_list = [];
-%                end
-%        
-%                % Remove ignored trials from trial idx list
-%                trial_idxs = setdiff(trial_idxs, trial_ignr_list);
-%
-%                % Skip ROI if there are at most 2 trials
-%                if length(trial_idxs) <= 2
-%                    continue;
-%                end
-%
-%                % Loop through each trial                
-%                for tr_idx=trial_idxs        
-%                    trial_data = data.align.trial{tr_idx};
-%                    raw_trial_data = data.raw.trial{tr_idx};
-%
-%                    % Store the camera framerate
-%                    all_Fs(end+1) = trial_data.camera_framerate;
-%                    cur_fov_Fs(end + 1) = trial_data.camera_framerate;
-%
-%                    % Grab the subthreshold Vm
-%                    % Chop the respective frames
-%                    cur_trace_ws = trial_data.spike_info375.trace_ws(roi_idx, front_frame_drop:back_frame_drop);
-%                    [baseline, coeff] = Multi_func.exp_fit_Fx(cur_trace_ws', round(trial_data.camera_framerate));
-%                    detrend_subVm = cur_trace_ws - baseline;
-%                    cur_fov_subVm = horzcat_pad(cur_fov_subVm, detrend_subVm');
-%
-%                    % Store all of the timestamp info
-%                    stim_start = raw_trial_data.raw_stimulation_time(1);
-%                    cur_fov_stim_time = horzcat_pad(cur_fov_stim_time, raw_trial_data.raw_stimulation_time - stim_start);
-%                    cur_fov_trace_time = horzcat_pad(cur_fov_trace_time, trial_data.camera_frame_time(front_frame_drop:back_frame_drop) - stim_start);
-%                    
-%                    % Save the phases for all trials
-%                    [wt, f] = get_power_spec(detrend_subVm', mean(cur_fov_Fs, 'omitnan'));                   
-%                    cur_fov_fourcoeff(:, :, end + 1) = wt;
-%                    
-%                    % Save the hilbert transform signal
-%
-%                    [filt_sig] = filt_data(detrend_subVm', [1:1:150] , mean(cur_fov_Fs, 'omitnan'));
-%                    cur_fov_filtsig(:, :, end + 1) = filt_sig;
-%
-%                end % End looping through each neuron
-%            end
-%            
-%            % Skip rest of the calculations if the subthreshold Vm is nan
-%            if sum(isnan(cur_fov_subVm(:))) > 0 || isempty(cur_fov_subVm)
-%                continue;
-%            end
-%
-%            % Plot the subthreshold heatmap, and then the coherence
-%            figure('Renderer', 'Painters', 'Position', [200 200 900 700]);
-%            tiledlayout(2, 1, 'TileSpacing', 'compact', 'Padding', 'compact');
-%            nexttile;
-%            surface(nanmean(cur_fov_trace_time, 2), 1:size(cur_fov_subVm, 2), cur_fov_subVm', 'CDataMapping', 'scaled', 'FaceColor', 'texturemap', 'edgecolor', 'none');
-%            Multi_func.set_default_axis(gca);
-%            xlabel('Time from onset(s)');
-%            ylabel('Trial #');
-%            xlim([-1, 2.05]);
-%            title('Sub Vm');
-%            
-%            nexttile;
-%            itc_map = abs(mean(cur_fov_fourcoeff./abs(cur_fov_fourcoeff), 3, 'omitnan'));
-%            %itc_map = exp(1i.*angle(cur_fov_fourcoeff));
-%            %itc_map = abs(mean(itc_map, 3, 'omitnan'));
-%            surface(nanmean(cur_fov_trace_time, 2), f, itc_map, 'CDataMapping', 'scaled', 'FaceColor', 'texturemap', 'edgecolor', 'none' );
-%            xlim([-1, 2.05]);
-%            xlabel('Time from onset(s)');
-%            ylabel('Frequency (Hz)');
-%            ylim([0 20]);
-%            a = colorbar;    
-%            a.Label.String = 'ITC';
-%            Multi_func.set_default_axis(gca);
-%            title('ITC');
-%
-%            %nexttile;
-%            %% One method has NaNs and the other has zeroes
-%            %% Either one seems very close
-%            %%itc_map_hil = abs(mean(cur_fov_filtsig./abs(cur_fov_filtsig), 3, 'omitnan'));
-%            %itc_map_hil = exp(1i.*angle(cur_fov_filtsig));
-%            %itc_map_hil = abs(mean(itc_map_hil, 3, 'omitnan'));
-%
-%            %surface(nanmean(cur_fov_trace_time, 2), [1:1:150], itc_map_hil, 'CDataMapping', 'scaled', 'FaceColor', 'texturemap', 'edgecolor', 'none' );
-%            %xlim([-1, 2.05]);
-%            %xlabel('Time from onset(s)');
-%            %ylabel('Frequency (Hz)');
-%            %a = colorbar;    
-%            %a.Label.String = 'ITC';
-%            %Multi_func.set_default_axis(gca);
-%            %title('ITC from Hilbert');
-%            sgtitle(matfile, 'Interpreter', 'none');
-%            
-%            % Look at rose plot for 2.2 sec time point, 1817 idx
-%            %data_line = cur_fov_fourcoeff(35, 1817, :);
-%            %figure;
-%            %plot(squeeze(angle(data_line)));
-%            
-%
-%            % Average for each neuron and save the subthreshold Vm
-%            temp = data_bystim.(f_stim).neuron_Vm;
-%            data_bystim.(f_stim).neuron_Vm = horzcat_pad(temp, nanmean(cur_fov_subVm, 2));
-%            % Store the timestamp data
-%            temp = data_bystim.(f_stim).stim_timestamps;
-%            data_bystim.(f_stim).stim_timestamps = horzcat_pad(temp, nanmean(cur_fov_stim_time, 2));
-%            temp = data_bystim.(f_stim).trace_timestamps;
-%            data_bystim.(f_stim).trace_timestamps = horzcat_pad(temp, nanmean(cur_fov_trace_time, 2));
-%            
-%            % Calculate and save frequency data
-%            [wt, f] = get_power_spec(nanmean(cur_fov_subVm, 2)', nanmean(cur_fov_Fs));
-%            temp = data_bystim.(f_stim).neuron_spec_power;
-%            data_bystim.(f_stim).neuron_spec_power = cat(3, temp, wt);
-%            temp = data_bystim.(f_stim).neuron_spec_freq;   
-%            data_bystim.(f_stim).neuron_spec_freq = cat(3, temp, f);
-%
-%        end % End looping through FOVs of a condition
-%    end
-%
-%    % Save the VM to the specific region
-%    region_data.(f_region).data_bystim = data_bystim;
-%end
+% Flag for neurons that have the low frequency oscillations
+sep_neurons_by_freq = 0;
+use_low_freq = 1; % 1 to calculate itc for low freq neurons
 
 %% Read in the saved pv data and perform analysis
 save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'pv_data.mat'];
@@ -224,6 +45,184 @@ if all_regions == 1
 end
 
 disp('Finished Loading');
+
+%% Identify low frequency oscillations
+if sep_neurons_by_freq == 1
+    % Determine neurons that do or do not have low frequency oscillations
+    for f_region = fieldnames(region_data)'
+        f_region = f_region{1};
+        for f_stim = fieldnames(region_data.(f_region))'
+            f_stim = f_stim{1};
+            region_data.(f_region).(f_stim).has_low_freq = Multi_func.get_low_freq_neurons(region_data.(f_region).(f_stim));
+        end
+    end
+end
+
+
+%% Calculate the ITC for all regions and stimulation frequencies
+freq_idx = Multi_func.theta_range(1):Multi_func.theta_range(2); 
+for f_region = fieldnames(region_data)'
+    f_region = f_region{1};
+    data_bystim = region_data.(f_region);
+
+    stims = fieldnames(data_bystim);
+
+    % Loop through stimulation frequencies
+    for f_stim=stims'
+        f_stim = f_stim{1};
+        
+        % Specify timepoints within trial
+        timeline = nanmean(region_data.(f_region).(f_stim).trace_timestamps, 2);
+        post_ran_idx = find(timeline >= 1 & timeline <= 1.5);
+        base_idx = find(timeline < 0);
+
+        % Store each neuron's base and post stimulation ITC for comparison
+        nr_base_itc = [];
+        nr_post_itc = [];
+        
+        neuron_trial_map = {};
+
+        % Loop through each neuron
+        for neuron=1:length(data_bystim.(f_stim).neuron_hilbfilt)
+            
+            if sep_neurons_by_freq == 1 && data_bystim.(f_stim).has_low_freq(neuron) == ~use_low_freq
+                disp('Skipped');
+                continue;                
+            end
+
+            cur_fourcoeff = data_bystim.(f_stim).neuron_hilbfilt{neuron};
+            itc_map = abs(mean(cur_fourcoeff./abs(cur_fourcoeff), 3, 'omitnan'));
+ 
+            % Append neuron's ITC values to population wide
+            nr_base_itc(end + 1) = mean( itc_map(freq_idx, base_idx), 'all');
+            nr_post_itc(end + 1) = mean( itc_map(freq_idx, post_ran_idx), 'all');
+            
+            % Get the trial's Vm for current neuron
+            neuron_trial_map{end + 1} = data_bystim.(f_stim).all_trial_rawVm{neuron};
+        end        
+
+        % Sort neurons based on the ITC value after stimulation period
+        [~, sort_i] = sort(nr_post_itc);
+        sort_i = flip(sort_i);
+        sorted_trials = neuron_trial_map(sort_i);
+        nr_base_itc = nr_base_itc(sort_i);
+        nr_post_itc = nr_post_itc(sort_i);
+
+        % Store the trial maps back into the structures
+        region_data.(f_region).(f_stim).itc_sorted_neuron_trials_map = sorted_trials;
+        region_data.(f_region).(f_stim).itc_neuron_baseline = nr_base_itc;
+        region_data.(f_region).(f_stim).itc_neuron_post = nr_post_itc;
+    end
+end
+
+%% Plot the trial heatmap for all brain regions and stimulation frequencies
+for f_region = fieldnames(region_data)'
+    f_region = f_region{1};
+    data_bystim = region_data.(f_region); %
+
+    stims = fieldnames(data_bystim);
+    % Loop through stimulation frequencies
+    for f_stim=stims'
+        f_stim = f_stim{1};
+        
+        neuron_bound = [0.5];
+        sorted_trials = data_bystim.(f_stim).itc_sorted_neuron_trials_map;
+        %Loop through the trials to create the bounds
+        for i=1:length(sorted_trials)
+            neuron_bound(end + 1) = neuron_bound(end) + size(sorted_trials{i}, 2);
+        end
+        
+        trial_map = [sorted_trials{:}];
+
+        % Plot all of the neurons Vm mapping sorted by ITC
+        figure('Renderer', 'Painters', 'Units', 'centimeters', 'Position', [4 20 21.09 27.94]);
+        
+        imagesc('XData', timeline, 'YData', 1:size(trial_map, 2) , 'CData', trial_map');
+        hold on;
+        yline(neuron_bound);
+        
+        text_y = 0.5 + [neuron_bound(1:end - 1) + neuron_bound(2:end)]/2;
+        % Plot the Post stim ITC values
+        for i=1:length(data_bystim.(f_stim).itc_neuron_post)
+            text(2.2, text_y(i), num2str(data_bystim.(f_stim).itc_neuron_post(i)));
+        end
+     
+        % Indicate if plotting for those with low frequency neuron
+        title_ext = '';
+        if sep_neurons_by_freq == 1 && use_low_freq == 1
+            title_ext = 'Low Frequency Neurons';
+        elseif sep_neurons_by_freq == 1 && use_low_freq == 0
+            title_ext = 'non-low freq neurons';
+        end
+
+        Multi_func.set_default_axis(gca);
+        
+        title([f_region ' ' f_stim ' ' title_ext], 'Interpreter', 'none');
+        xlim([-1, 2.5]);
+        
+        saveas(gcf, [figure_path 'ITC/Vm_heatmap_itc_sorted_' title_ext '_' f_region '_' f_stim '.png']);
+        saveas(gcf, [figure_path 'ITC/Vm_heatmap_itc_sorted_' title_ext '_' f_region '_' f_stim '.pdf']);
+
+    end
+end
+
+%% Plot violins to comapre baseline and post across all regions and stimulation frequency
+for f_region = fieldnames(region_data)'
+    f_region = f_region{1};
+    stims = fieldnames(region_data.(f_region))
+    % Start the figures for each brain region
+    figure('Renderer', 'Painters', 'Units', 'centimeters', 'Position', [4 20 21.59 27.94]);
+    tiledlayout(length(stims), 1, 'TileSpacing', 'compact', 'Padding', 'compact', 'Units', 'centimeters', 'InnerPosition', [1, 1, 7, 12]);
+    
+    for f_stim = stims'
+        f_stim = f_stim{1};
+        nexttile;
+        popul_data = region_data.(f_region).(f_stim);
+                
+        data = [popul_data.itc_neuron_baseline, popul_data.itc_neuron_post];
+        labels = [repmat({'Base'}, 1, length(popul_data.itc_neuron_baseline)), repmat({'Post'}, 1, length(popul_data.itc_neuron_post))];
+        ViolinOpts = Multi_func.get_default_violin();
+        violins = violinplot(data, labels, 'GroupOrder', {'Base', 'Post'}, ViolinOpts);
+        violins(1).ViolinColor = {Multi_func.base_color};
+        violins(2).ViolinColor = {Multi_func.post_color};
+        hold on;
+        
+        % Conditionally color lines between violinplots
+        for i=1:length(popul_data.itc_neuron_post)
+            color = [0 0 0 0.4];
+        
+            % Neuron increased their ITC (considered ITC neuron)
+            if popul_data.itc_neuron_post(i) > popul_data.itc_neuron_baseline(i)*1.20
+                color = [[30, 2, 237]/255, 0.4];
+                
+            % Neuron had a decrease in their ITC
+            elseif popul_data.itc_neuron_post(i) < popul_data.itc_neuron_baseline(i)*0.8
+                color = [[235, 5, 28]/255, 0.4];
+            end
+        
+            plot([1 2], [popul_data.itc_neuron_baseline(i), popul_data.itc_neuron_post(i)], '-', 'Color', color);
+            hold on
+        end
+
+        % Setting the axis default
+        Multi_func.set_default_axis(gca);
+    
+        % Indicate if plotting for those with low frequency neuron
+        title_ext = '';
+        if sep_neurons_by_freq == 1 && use_low_freq == 1
+            title_ext = 'Low Frequency Neurons';
+        elseif sep_neurons_by_freq == 1 && use_low_freq == 0
+            title_ext = 'non-low freq neurons';
+        end
+        
+        title([f_region ' ' f_stim], 'Interpreter', 'none');
+    end
+    sgtitle(['Base to Post ITC Comparison' title_ext ' neurons']);
+
+    saveas(gcf, [figure_path 'ITC/Base_vs_Post_ITC_for_' title_ext '_' f_region '.png']);
+    saveas(gcf, [figure_path 'ITC/Base_vs_Post_ITC_for_' title_ext '_' f_region '.pdf']);
+
+end
 
 %% Calculate all of the ITCs for 40Hz M1 region neurons, and sort by the post ITRC  
 f_region = 'r_M1';
@@ -261,8 +260,8 @@ sorted_trials = neuron_trial_map(sort_i);
 nr_base_itc = nr_base_itc(sort_i);
 nr_post_itc = nr_post_itc(sort_i);
 
-%%
 
+%% Plot all of the neuron's in sorting post-stim ITC order
 neuron_bound = [0];
 %Loop through the trials to create the bounds
 for i=1:length(sorted_trials)
@@ -287,7 +286,7 @@ title('Sorted Trial Map');
 Multi_func.set_default_axis(gca);
 xlim([-1, 2.5]);
 
-%% Plot violins to compare the base vs post stim ITC values for all neurons
+%% Plot violins to compare the base vs post stim ITC values for 40Hz motor cortex
 figure('Renderer', 'Painters', 'Position', [200 200 700 700]);
 data = [nr_base_itc, nr_post_itc];
 labels = [repmat({'Base'}, 1, length(nr_base_itc)), repmat({'Post'}, 1, length(nr_post_itc))];
