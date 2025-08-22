45386c34bbc1e575dbff6c408b5f388d79f3e5cb
Pier Fier <fabrispierre@gmail.com>
2025-08-22 15:58:41 -0400

    Modified 'spike_to_pulse.m' to specify whether to load in data without the beginning 200ms

--- spike_to_pulse.m
+++ spike_to_pulse.m
@@ -23,6 +23,22 @@ figure_path = [server_root_path 'Pierre Fabris' f 'PV Project' f 'Plots' f];
 % CSV file to determine which trials to ignore
 ignore_trial_dict = Multi_func.csv_to_struct([local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f ...
                                        'Stim Recordings' f 'Data_Config' f 'byvis_ignore.csv']);
+%% Load region_data
+if ~isfolder(server_root_path)
+    disp('Server rootpath does not exist!!!');
+    return;
+end
+
+% Read in the saved pv data and perform analysis
+if ~exclude_200ms
+    save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'pv_data.mat'];
+else
+    save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'pv_data_ex200.mat'];
+    %save_all_data_file = [local_root_path 'Pierre Fabris' f 'PV DBS neocortex' f 'Interm_Data' f 'ca1_data.mat'];
+end
+%Load the data
+load(save_all_data_file);
+
 
 %% Loop through each region
 for f_region = fieldnames(region_data)'
